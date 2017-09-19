f71f1d01028f07b6181b48731d198dfbc150e280
Joseph J San Nicolas <jsannicolas@gnx1.rd.unr.edu>
2017-09-19 16:25:12 -0700

    added PA3

---
+++ PA3/include/object.h
@@ -0,0 +1,29 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include "graphics_headers.h"
+
+class Object
+{
+  public:
+    Object();
+    ~Object();
+    void Update(unsigned int dt, bool reverse, bool pause);
+    void Render();
+
+    glm::mat4 GetModel();
+
+  private:
+    glm::mat4 model;
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+
+    float angle;
+	float orbit;
+	float rotate;
+};
+
+#endif /* OBJECT_H */
